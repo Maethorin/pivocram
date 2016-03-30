21a6e05e04e2e05f3059e4802c611d02ac4b72af
Márcio Duarte <maethorin@gmail.com>
2016-03-30 01:27:41 -0300

    Creating Flask App Structure

---
+++ app/initialize.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+import os
+
+from flask import Flask, send_from_directory, g, request
+
+import api, models
+
+web_app = Flask(__name__)
+web_app.config.from_object(os.environ['APP_SETTINGS'])
+app_directory = os.path.join(os.getcwd(), 'app')
+template_directory = os.path.join(app_directory, 'templates')
+
+api.create_api(web_app)
+
+DOMAIN = 'pivocram.com'
+if web_app.config['DEVELOPMENT']:
+    DOMAIN = '127.0.0.1:8000'
+
+
+@web_app.before_request
+def before_request():
+    """
+    Check if the token received in header is still a valid token from an user.
+    """
+    token = request.headers.get('XSRF-TOKEN', None)
+    user = None
+    if token:
+        user = models.User.check_auth_token(token)
+    if user:
+        g.user = user
+
+
+@web_app.after_request
+def add_cache_header(response):
+    """
+    Add response headers for Cache Control
+    """
+    response.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+
+
+@web_app.after_request
+def add_access_control_header(response):
+    """
+    Add response headers for CORS
+    """
+    response.headers['Access-Control-Allow-Origin'] = 'http://{}'.format(DOMAIN)
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization,Set-Cookie,XSRF-TOKEN'
+    response.headers['Access-Control-Expose-Headers'] = 'Content-Type,Authorization,Set-Cookie,XSRF-TOKEN'
+    response.headers['Access-Control-Allow-Methods'] = ','.join(['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'])
+    response.headers['Access-Control-Max-Age'] = 21600
+    return response
+
+
+@web_app.after_request
+def add_token_header(response):
+    """
+    Refresh the token header if the user is in flask global or delete the response header if not
+    """
+    user = g.get('user', None)
+    if 'XSRF-TOKEN' in response.headers:
+        del response.headers['XSRF-TOKEN']
+    if user is not None:
+        token = user.generate_auth_token()
+        response.headers['XSRF-TOKEN'] = token.decode('ascii')
+    return response
+
+
+@web_app.route('/', methods=['GET', 'POST'])
+def index():
+    """
+    Return the index.html template
+    """
+    return send_from_directory(template_directory, 'index.html')
+
+
+@web_app.route('/template/<path:template_path>', methods=['GET', 'POST'])
+def angular_template(template_path):
+    """
+    Return a file in the template folder to be used as a template for angular.
+    """
+    return send_from_directory(template_directory, template_path)
+
+
+@web_app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    """
+    Return the Favicon
+    """
+    return web_app.send_static_file("img/favicon.ico")
+
+
+def run():
+    """
+    Run the flask app in a develpment enviroment
+    """
+    web_app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ tests/unit/test_initialize.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from tests import base
+from app import initialize
+
+
+class InitializeTest(base.TestCase):
+    def test_should_add_cache_headers_in_response(self):
+        response = self.mock.MagicMock(headers={})
+        initialize.add_cache_header(response)
+        response.headers.should.be.equal({
+            'Cache-Control': 'no-cache, no-store, must-revalidate',
+            'Expires': '0',
+            'Pragma': 'no-cache'
+        })
+
+    def test_should_add_access_control_headers_in_response(self):
+        response = self.mock.MagicMock(headers={})
+        initialize.add_access_control_header(response)
+        response.headers.should.be.equal({
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Headers': 'Content-Type,Authorization,Set-Cookie,XSRF-TOKEN',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+            'Access-Control-Allow-Origin': 'http://pivocram.com',
+            'Access-Control-Expose-Headers': 'Content-Type,Authorization,Set-Cookie,XSRF-TOKEN',
+            'Access-Control-Max-Age': 21600
+        })
+
+    @base.TestCase.mock.patch('app.initialize.g')
+    def test_should_add_token_headers_in_reponse_when_g_have_an_user(self, g_mock):
+        user_mock = self.mock.MagicMock()
+        user_mock.generate_auth_token.return_value = 'GENERATED-TOKEN'
+        g_mock.get.return_value = user_mock
+        response = self.mock.MagicMock(headers={})
+        initialize.add_token_header(response)
+        response.headers.should.be.equal({'XSRF-TOKEN': 'GENERATED-TOKEN'})
+
+    @base.TestCase.mock.patch('app.initialize.g')
+    def test_should_remove_token_headers_in_response_when_g_doesnt_have_an_user(self, g_mock):
+        g_mock.get.return_value = None
+        response = self.mock.MagicMock(headers={'XSRF-TOKEN': 'SOME-TOKEN'})
+        initialize.add_token_header(response)
+        response.headers.should.be.equal({})
