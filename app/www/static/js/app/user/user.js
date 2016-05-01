282a96c1a8662b0ab036b21353ab33ea2342afb1
Márcio Duarte <maethorin@gmail.com>
2016-05-01 15:17:10 -0300

    Changing password

--- app/www/static/js/app/user/user.js
+++ app/www/static/js/app/user/user.js
@@ -1,23 +1,38 @@
 'use strict';
 
 angular.module('pivocram.user', [])
+    .directive('passwordMatch', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+                ctrl.$validators.passwordMatch = function(modelValue, viewValue) {
+                    if (scope.password) {
+                        return scope.password.new == (viewValue || ctrl.$viewValue);
+                    }
+                };
+            }
+        };
+    })
     .config(['$routeProvider', function($routeProvider) {
         var templatesFolder = '/templates';
         $routeProvider
             .when('/user/show', {
-              templateUrl: '{0}/user/show.html'.format([templatesFolder]),
-              controller: 'ShowUserController'
+                templateUrl: '{0}/user/show.html'.format([templatesFolder]),
+                controller: 'ShowUserController'
             })
             .when('/user/create', {
-              templateUrl: '{0}/user/create.html'.format([templatesFolder]),
-              controller: 'CreateUserController'
+                templateUrl: '{0}/user/create.html'.format([templatesFolder]),
+                controller: 'CreateUserController'
             });
     }])
     .controller("ShowUserController", ['$scope', 'User', function($scope, User) {
         $scope.user = User.get();
-        $scope.updateFail = false;
         $scope.errorMessage = null;
+        $scope.updateFail = false;
         $scope.updateComplete = false;
+        $scope.changePasswordFail = false;
+        $scope.changePasswordComplete = false;
+        $scope.password = {new: null, confirm: null};
         $scope.updateUser = function() {
             $scope.updateFail = false;
             $scope.errorMessage = null;
@@ -36,5 +51,26 @@ angular.module('pivocram.user', [])
                     $scope.errorMessage = resp.data.errorMessage;
                 }
             );
+        };
+        $scope.changePassword = function() {
+            $scope.changePasswordFail = false;
+            $scope.errorMessage = null;
+            $scope.changePasswordComplete = false;
+            if ($scope.formPassword.$invalid) {
+                $scope.changePasswordFail = true;
+                $scope.errorMessage = 'invalid form data';
+                $scope.password = {new: null, confirm: null};
+                return false;
+            }
+            new User({password: $scope.password.new}).$update().then(
+                function() {
+                    $scope.changePasswordComplete = true;
+                },
+                function(resp) {
+                    $scope.changePasswordFail = true;
+                    $scope.errorMessage = resp.data.errorMessage;
+                }
+            );
+            $scope.password = {new: null, confirm: null};
         };
     }]);
--- tests/js_unit/user.tests.js
+++ tests/js_unit/user.tests.js
@@ -1,16 +1,47 @@
 describe('User module', function() {
-    var $route, appConfig, $controller, $rootScope, $httpBackend, $location, AuthService, User;
+    var $route, appConfig, $controller, $rootScope, $httpBackend, $location, $compile, AuthService, User;
     beforeEach(module('pivocram'));
-    beforeEach(inject(function(_$route_, _appConfig_, _$controller_, _$rootScope_, _$httpBackend_, _$location_, _AuthService_, _User_) {
+    beforeEach(inject(function(_$route_, _appConfig_, _$controller_, _$rootScope_, _$httpBackend_, _$location_, _$compile_, _AuthService_, _User_) {
         $route = _$route_;
         appConfig = _appConfig_;
         $controller = _$controller_;
         $rootScope = _$rootScope_;
         $httpBackend = _$httpBackend_;
         $location = _$location_;
+        $compile = _$compile_;
         AuthService = _AuthService_;
         User = _User_;
     }));
+    describe('password-match', function() {
+        var $scope;
+        beforeEach(function() {
+            $scope = $rootScope.$new();
+            $scope.password = {new: '1234', confirm: null};
+            var html = [
+                '<form name="form" novalidate>',
+                '<input type="password" ng-model="confirmPassword" name="confirmPassword" password-match />',
+                '</form>'
+            ].join('');
+            $compile(html)($scope);
+        });
+        it('should have for password confirmation', function() {
+            expect($scope.form.confirmPassword.$validators.passwordMatch).toBeDefined();
+        });
+        it('should set invalid if passwords does not match', function() {
+            $scope.form.confirmPassword.$setViewValue('1222');
+            expect($scope.form.confirmPassword.$validators.passwordMatch()).toBeFalsy();
+        });
+        it('should set invalid if passwords does not match passing to function', function() {
+            expect($scope.form.confirmPassword.$validators.passwordMatch(null, '1222')).toBeFalsy();
+        });
+        it('should set valid if passwords match', function() {
+            $scope.form.confirmPassword.$setViewValue('1234');
+            expect($scope.form.confirmPassword.$validators.passwordMatch()).toBeTruthy();
+        });
+        it('should set valid if passwords match passing to function', function() {
+            expect($scope.form.confirmPassword.$validators.passwordMatch(null, '1234')).toBeTruthy();
+        });
+    });
     describe('Routes', function() {
         it('should have route to user profile', function() {
             expect($route.routes['/user/show']).toBeDefined();
@@ -49,6 +80,10 @@ describe('User module', function() {
             expect($scope.updateFail).toBeDefined();
             expect($scope.updateFail).toBeFalsy();
         });
+        it('should have flag to indicate password change failed', function() {
+            expect($scope.changePasswordFail).toBeDefined();
+            expect($scope.changePasswordFail).toBeFalsy();
+        });
         it('should have errorMessage', function() {
             expect($scope.errorMessage).toBeDefined();
         });
@@ -56,11 +91,21 @@ describe('User module', function() {
             expect($scope.updateComplete).toBeDefined();
             expect($scope.updateComplete).toBeFalsy();
         });
+        it('should have flag to indicate password change complete', function() {
+            expect($scope.changePasswordComplete).toBeDefined();
+            expect($scope.changePasswordComplete).toBeFalsy();
+        });
         it('should have function to update user', function() {
             expect($scope.updateUser).toBeDefined();
         });
+        it('should have newPassword', function() {
+            expect($scope.password).toEqual({new: null, confirm: null});
+        });
+        it('should have function to password change', function() {
+            expect($scope.changePassword).toBeDefined();
+        });
     });
-    describe('Updating user', function() {
+    describe('Updating user name and pivotal token', function() {
         var $scope, user;
         beforeEach(function() {
             $scope = $rootScope.$new();
@@ -112,6 +157,80 @@ describe('User module', function() {
             $httpBackend.flush();
             expect($scope.updateFail).toBeTruthy();
             expect($scope.errorMessage).toBe('Invalid Data');
+        });
+    });
+    describe('Changing password', function() {
+        var $scope, user;
+        beforeEach(function() {
+            $scope = $rootScope.$new();
+            $scope.formPassword = {
+                $invalid: false
+            };
+            user = {name: 'User Name', pivotalToken: 'PIVOTAL-TOKEN', email: 'user@test.com'};
+            $httpBackend.expect('GET', '{0}/api/me'.format([appConfig.backendURL])).respond(200, user);
+            $controller('ShowUserController', {$scope: $scope});
+            $scope.password = {new: 'new-password', confirm: null};
+        });
+        it('should flag change fail if form is invalid', function() {
+            $scope.formPassword.$invalid = true;
+            $scope.changePassword();
+            expect($scope.changePasswordFail).toBeTruthy();
+            expect($scope.errorMessage).toBe('invalid form data');
+        });
+        it('should clear form form is invalid', function() {
+            $scope.formPassword.$invalid = true;
+            $scope.changePassword();
+            expect($scope.password).toEqual({new: null, confirm: null});
+        });
+        it('should call resource if data is ok', function() {
+            $httpBackend.expect(
+                "PUT",
+                "{0}/api/me".format([appConfig.backendURL]),
+                {password: 'new-password'}
+            ).respond(200, {result: 'success'});
+            $scope.changePassword();
+            $httpBackend.flush();
+        });
+        it('should flag change is complete', function() {
+            $httpBackend.expect(
+                "PUT",
+                "{0}/api/me".format([appConfig.backendURL]),
+                {password: 'new-password'}
+            ).respond(200, {name: 'Updated User Name', email: 'test2@user.com', pivotalToken: 'TOO-KEN'});
+            $scope.changePassword();
+            $httpBackend.flush();
+            expect($scope.changePasswordComplete).toBeTruthy();
+        });
+        it('should clean model on complete', function() {
+            $httpBackend.expect(
+                "PUT",
+                "{0}/api/me".format([appConfig.backendURL]),
+                {password: 'new-password'}
+            ).respond(200, {name: 'Updated User Name', email: 'test2@user.com', pivotalToken: 'TOO-KEN'});
+            $scope.changePassword();
+            $httpBackend.flush();
+            expect($scope.password).toEqual({new: null, confirm: null});
+        });
+        it('should flag change is failed on error', function() {
+            $httpBackend.expect(
+                "PUT",
+                "{0}/api/me".format([appConfig.backendURL]),
+                {password: 'new-password'}
+            ).respond(400, {errorMessage: 'Invalid Data'});
+            $scope.changePassword();
+            $httpBackend.flush();
+            expect($scope.changePasswordFail).toBeTruthy();
+            expect($scope.errorMessage).toBe('Invalid Data');
+        });
+        it('should clean password on error', function() {
+            $httpBackend.expect(
+                "PUT",
+                "{0}/api/me".format([appConfig.backendURL]),
+                {password: 'new-password'}
+            ).respond(400, {errorMessage: 'Invalid Data'});
+            $scope.changePassword();
+            $httpBackend.flush();
+            expect($scope.password).toEqual({new: null, confirm: null});
         });
     });
 });
